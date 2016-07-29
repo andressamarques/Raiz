b89a7e97e79fd533192051edb9b726a94e1c1141
andressamarques <andressa.alves.aa.457@gmail.com>
2016-07-29 08:42:38 -0300

    Raiz Quadrada
    Trabalho Antoanna (é o brilhor do teu olhar)
    Descobrir a raiz quadrada de um número.

---
+++ Raiz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número:");
+            int.Parse(Console.ReadLine());
+        }
+
+        static int raiz(int n)
+        {
+            
+            int i = 1;
+            int dif = n - i;
+            int termos = 1;
+            while (dif != 0)
+            {
+                n -= i;
+                i += 2;
+                dif = n - i;
+                termos++;
+            }
+            return termos;
+
+        }
+
+    }
+}
